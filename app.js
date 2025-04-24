a2897992000852ea70cb8af19e70354b0cf2459e
AhaD <abuahad35188@gmail.com>
2025-04-25 01:24:16 +0600

    yoyo and stagger prpperty

--- app.js
+++ app.js
@@ -13,14 +13,18 @@ gsap.from("#box2",{
      delay:2,
      rotate:360,
      borderRadius:50,
+     yoyo: true,
+     repeat:1
 })
 
 gsap.from("h1",{
      y:30,
      x:50,
+     color:"white",
      opacity:0,
      duration:2,
      delay:1,
      color:"white",
-     stagger:-1
+     stagger:-1,
+     repeat:-1
 })
