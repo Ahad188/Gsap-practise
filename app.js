206be20ed9de8714282f8f108c32d1d37f3d509e
AhaD <abuahad35188@gmail.com>
2025-04-25 01:16:33 +0600

    text animation :

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ console.log("hello Gsap")
 gsap.to("#box1",{
      x:900,
      duration:2,
-     dealy:2,
+     delay:2,
+     rotate:380,
+     backgroundColor:"blue",
 })
 gsap.from("#box2",{
      x:900,
      duration:2,
-     dealy:2,
+     delay:2,
+     rotate:360,
+     borderRadius:50,
+})
+
+gsap.from("h1",{
+     y:30,
+     x:50,
+     opacity:0,
+     duration:2,
+     delay:1,
+     color:"white",
+     stagger:-1
 })
